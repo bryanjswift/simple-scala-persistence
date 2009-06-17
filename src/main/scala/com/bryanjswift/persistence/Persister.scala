7b5fd6c71fbbdaa0a01ee60a77aeac2c1fd73b29
Bryan J Swift <bryan.j.swift@gmail.com>
2009-06-16 23:22:02 -0400

    added get and search methods to the Persister trait

--- src/main/scala/com/bryanjswift/persistence/Persister.scala
+++ src/main/scala/com/bryanjswift/persistence/Persister.scala
@@ -4,4 +4,6 @@ import java.io.Serializable
 
 trait Persister {
 	def save(obj:Savable):Serializable
+	def get(table:String,id:Long):Savable
+	def search(table:String,field:String,value:Any):List[Savable]
 }
